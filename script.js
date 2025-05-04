e55ac3a359d0bd17297de2f2f0bfc3aabd679dd1
Sbarik-04 <souravbarik260@gmail.com>
2025-05-04 21:01:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+  
